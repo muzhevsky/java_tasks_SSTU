15c0553cc8fadb8b9e1fe2a595bc23af8721a139
privet_andrey_laptop <>
2023-04-22 19:26:36 +0400

    stream simplification and mysql connection

--- src/com/muzhevsky/main/Main.java
+++ src/com/muzhevsky/main/Main.java
@@ -10,30 +10,39 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.AbstractMap;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        var Student1 = new Student("d", 3,4,2,2);
-        var Student2 = new Student("c", 4,5,5);
-        var Student3 = new Student("b", 2,5,3);
-        var Student4 = new Student("a",4);
-        var Student5 = new Student("a",5);
+        var Student1 = new Student("ivan", 3,4,2,2);
+        var Student2 = new Student("nikita", 4,5,5);
+        var Student3 = new Student("sergei", 2,5,3);
+        var Student4 = new Student("nikolay",4);
+        var Student5 = new Student("leha",5);
 
 
+        var marks = new String[]{"F","D","C","B","A","S"};
         var test = Stream.of(Student1, Student2, Student3, Student4, Student5)
-                .map(student -> new AbstractMap.SimpleEntry<>(Math.floor(student.getAverageIntMark()), student))
-                .collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.mapping(Map.Entry::getValue, Collectors.toList())));
+                .collect(Collectors.groupingBy(student -> marks[(int)(student.getAverageIntMark())], Collectors.mapping(student -> student, Collectors.toList())));
+
 
         System.out.println(test);
 
-        for (var item : test.keySet()){
-            System.out.println(test.get(item));
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection=DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/office","root",""); // password requires
+        Statement statement = connection.createStatement();
+        var set = statement.executeQuery("select * from employee");
+        var metadata = set.getMetaData();
+        while(set.next()){
+            for(int i = 1; i <= metadata.getColumnCount(); i++)
+                System.out.println(set.getString(i));
         }
     }
 }
